d384782488f3d93351c36c0556e2d9bf572f5136
Niraj <nsvora@ucdavis.edu>
2015-10-06 19:38:34 -0700

    add scoring

--- server/controllers/matchController.js
+++ server/controllers/matchController.js
@@ -28,39 +28,52 @@ matchController.createMatch = function(req, res) {
     users: [
     {
       id: req.session.passport.user,
-      levelScore: 0,
+      levelScore: 100,
       totalScore: 0,
-      currentLevel: 0,
-      plays: 0,
-      fails: 0,
+      currentLevel: 1,
+      plays: false,
+      fails: false,
       won: false
     },
     {
       id: req.body.otherId,
-      levelScore: 0,
+      levelScore: 100,
       totalScore: 0,
-      currentLevel: 0,
-      plays: 0,
-      fails: 0,
+      currentLevel: 1,
+      plays: false,
+      fails: false,
       won: false
     }]
   });
 };
+
+matchController.updater = function(matchObject, req) {
+  for (var i = 0; i < matchObject.users.length; i++) {
+    if (matchObject.users[i].id === req.session.passport.user){
+      matchObject.users[i].plays += req.body.play;
+      matchObject.users[i].fails += req.body.fail;
+      matchObject.users[i].levelScore -= (req.body.play * 5 + req.body.fail * 10);
+
+      if(matchObject.users[i].currentLevel !== req.body.currentLevel) {
+        matchObject.users[i].totalScore += matchObject.users[i].levelScore;
+        matchObject.users[i].currentLevel = req.body.currentLevel;
+        matchObject.users[i].levelScore = 100 + req.body.currentLevel * 20;        
+      }
+
+    } else {
+      matchObject.users[i].won = req.body.forfeit;
+    }
+  }
+  matchObject.open = !req.body.forfeit;
+  return matchObject;
+}
 
 matchController.updateMatch = function(req, res) {
   Match.findById(req.params.id, function(err, match) {
     if(err){
       res.status(404).send(err);
     } else {
-      for (var i = 0; i < match.users.length; i++) {
-        if (match.users[i].id === req.session.passport.user){
-          match.users[i].plays = req.body.plays;
-          match.users[i].fails = req.body.fails;
-        } else {
-          match.users[i].won = req.body.forfeit;
-        }
-      }
-      match.open = !req.body.forfeit;
+      match = matchController.updater(match, req);
       match.save(function(err, match) {
         if (err) {
           res.status(404).send(err);
