8faa40e8f84b7d7c47ac671498cc02dd0363bd04
dkoleary88 <dan@dko.io>
2015-10-07 10:45:46 -0700

    Rename internal object

--- client/app/factories/httpFactory.js
+++ client/app/factories/httpFactory.js
@@ -1,9 +1,9 @@
 app.factory( 'httpFactory', [ '$http', function ( $http ) {
 
   //Sends request to server and retrieves sequencer for given level
-  var requests = {};
+  var httpFact = {};
 
-  requests.getSequencer = function ( level, callback ) {
+  httpFact.getSequencer = function ( level, callback ) {
     return $http.get( '/levels/' + level.toString( ) )
       .then( function( response ) {
         if( callback ) {
@@ -12,7 +12,7 @@ app.factory( 'httpFactory', [ '$http', function ( $http ) {
       });
   };
 
-  requests.postSequencer = function ( level, stringifiedSequencer, callback ) {
+  httpFact.postSequencer = function ( level, stringifiedSequencer, callback ) {
     return $http.post( '/levels/', { level: level, data: stringifiedSequencer } )
       .then( function ( response ) {
         if( callback ) {
@@ -21,7 +21,7 @@ app.factory( 'httpFactory', [ '$http', function ( $http ) {
       });
   };
 
-  requests.putSequencer = function ( level, stringifiedSequencer, callback ) {
+  httpFact.putSequencer = function ( level, stringifiedSequencer, callback ) {
     return $http.put( '/levels/', { level: level, data: stringifiedSequencer } )
       .then( function ( response ) {
         if( callback ) {
@@ -30,7 +30,7 @@ app.factory( 'httpFactory', [ '$http', function ( $http ) {
       });
   };
 
-  requests.loginUser = function ( user, callback ) {
+  httpFact.loginUser = function ( user, callback ) {
     return $http.post( '/login', { username: user.username, password: user.password } )
       .then( function ( response ) {
         if ( callback ) {
@@ -39,7 +39,7 @@ app.factory( 'httpFactory', [ '$http', function ( $http ) {
       });
   };
 
-  requests.signupUser = function ( user, callback ) {
+  httpFact.signupUser = function ( user, callback ) {
     return $http.post( '/signup', { username: user.username, password: user.password } )
       .then( function ( response ) {
         if ( callback ) {
@@ -49,7 +49,7 @@ app.factory( 'httpFactory', [ '$http', function ( $http ) {
   };
 
   //TERRIBLY NAMED - THIS UPDATES A USER'S BEST LEVEL, NOT THE LEVEL ITSELF
-  requests.updateLevel = function ( user, callback ) {
+  httpFact.updateLevel = function ( user, callback ) {
     return $http.put( '/users', { username: user.username, level: user.level } )
       .then( function ( response ) {
         if ( callback ) {
@@ -58,7 +58,7 @@ app.factory( 'httpFactory', [ '$http', function ( $http ) {
       });
   };
 
-  requests.getUser = function ( callback ) {
+  httpFact.getUser = function ( callback ) {
     return $http.get( '/users' )
       .then( function ( response ) {
         if( callback ) {
@@ -67,7 +67,7 @@ app.factory( 'httpFactory', [ '$http', function ( $http ) {
       });
   };
 
-  requests.logout = function ( callback ) {
+  httpFact.logout = function ( callback ) {
     return $http.post( '/logout' )
       .then( function ( response ) {
         if ( callback ) {
@@ -76,6 +76,9 @@ app.factory( 'httpFactory', [ '$http', function ( $http ) {
     });
   };
 
-  return requests;
+  // New methods for multiplayer Beats with Friends
+  httpFact.
+
+  return httpFact;
 
 }]);
