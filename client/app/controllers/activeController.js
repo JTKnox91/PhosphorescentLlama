0dec1b73849349dcbc5edc44299113e1dfaee995
dkoleary88 <dan@dko.io>
2015-10-08 14:55:11 -0700

    Fix redirect after login and signup

--- client/app/controllers/activeController.js
+++ client/app/controllers/activeController.js
@@ -1,4 +1,24 @@
+<<<<<<< Updated upstream
 app.controller( 'ActiveController', ['$state', '$scope', 'httpFactory', '$rootScope', '$location', 'AuthFactory' , function ( $state, $scope, httpFactory, $rootScope, $location, AuthFactory ) {
+=======
+app.controller( 'ActiveController', ['$state', '$scope', 'httpFactory', '$rootScope', '$location', '$interval' , 'AuthFactory', function ( $state, $scope, httpFactory, $rootScope, $location, $interval, AuthFactory) {
+  $scope.minutes = 0;
+  $scope.seconds = '00';
+
+  var timer = $interval(function() {
+      if ($scope.seconds == '59') {
+        $scope.minutes++;
+        $scope.seconds = '00';
+      } else {
+        if( (Number($scope.seconds) + 1) < 10 ) {
+          $scope.seconds = '0' + (Number($scope.seconds) + 1);
+        } else {
+          $scope.seconds = (Number($scope.seconds) + 1);
+        }
+      }
+    }, 1000);
+
+>>>>>>> Stashed changes
   $scope.logout = function ( ) {
     AuthFactory.logout()
       .then(function (res) {
@@ -18,6 +38,19 @@ app.controller( 'ActiveController', ['$state', '$scope', 'httpFactory', '$rootSc
 
   $scope.submitMatch = function ( ) {
     $rootScope.$broadcast( 'submitMatch' );
+<<<<<<< Updated upstream
+=======
+    httpFactory.updateMatch($rootScope.currentMatchId.toString(), {
+      currentLevel: $rootscope.user.currentLevel,
+      fail: true
+    })
+    .then( function (res) {
+      console.log('Success');
+    })
+    .catch( function (error) {
+      console.error(error);
+    });
+>>>>>>> Stashed changes
   };
 
   $scope.playing = true;
